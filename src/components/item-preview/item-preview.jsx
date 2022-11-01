71960edbbb42ef1777765882c2457a65a8cab60a
Camilo <jcamilovelandiab@gmail.com>
2022-10-31 22:15:37 -0500

    use a links instead of navigate to improve SEO

--- src/components/item-preview/item-preview.jsx
+++ src/components/item-preview/item-preview.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NumericFormat } from 'react-number-format';
+import { Link } from 'react-router-dom';
 
 import Grid from '../grid/grid';
-import { NumericFormat } from 'react-number-format';
 import shippingImg from '../../assets/images/ic_shipping.png';
 
 import styles from './item-preview.module.scss';
 
 const ItemPreview = ({ item }) => {
 
-	const navigate = useNavigate();
-
-	const onClick = () => {
-		navigate(`${item.id}`);
-	}
-
 	return (
 		<div className={styles.item_preview}>
 			<Grid container>
-				<Grid item cols={8} style={styles.item_preview_details} onClick={onClick}>
-					<img src={item.picture} alt={item.title} className={styles.item_preview_image} />
+				<Grid item cols={8} style={styles.item_preview_details}>
+					<Link to={`${item.id}`}>
+						<img src={item.picture} alt={item.title} className={styles.item_preview_image} />
+					</Link>
 					<div className={styles.item_preview_description}>
 						<div className={styles.item_preview_description_amount}>
 							<NumericFormat
@@ -39,9 +35,11 @@ const ItemPreview = ({ item }) => {
 								)
 							}
 						</div>
-						<div className={styles.item_preview_description_title}>
-							{item.title}
-						</div>
+						<Link to={`${item.id}`}>
+							<div className={styles.item_preview_description_title}>
+								{item.title}
+							</div>
+						</Link>
 					</div>
 				</Grid>
 				<Grid item cols={2}>
